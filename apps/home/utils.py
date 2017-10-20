8293ede3b80c0d6cdb9a121fd6ff5ef1df7fa244
Z <763997136@qq.com>
2017-10-20 20:27:41 +0800

    删除了多余的文件

--- apps/home/utils.py
+++
@@ -1,9 +0,0 @@
-from flask import current_app
-
-
-def check_upload(filename):
-    if '.' not in filename:
-        return False
-    if filename.split('.')[-1].lower() not in current_app.config['ALLOWED_EXTENSIONS']:
-        return False
-    return True
--- manage.py
+++
@@ -1,7 +0,0 @@
-from config import DevConfig
-from apps import create_app
-
-app = create_app(DevConfig)
-
-if __name__ == '__main__':
-    app.run()
