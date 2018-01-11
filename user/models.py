2a8acf23e043702d17ec117de28fe3e7e006e80a
Z <763997136@qq.com>
2018-01-11 17:19:51 +0800

    用户关注模型

--- user/models.py
+++ user/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.User', verbose_name='用户')
     number = models.CharField(max_length=11, verbose_name='电话号码')
+    followers = models.ManyToManyField(
+        'UserProfile',
+        related_name='following',
+        blank=True,
+        verbose_name='关注TA的人'
+    )
 
     class Meta:
         verbose_name = '用户信息'
